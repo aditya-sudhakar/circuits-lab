98b1001b0c122d96be4b01bcd26cb34b00443734
Shashank Swaminathan <sh.swami235@gmail.com>
2020-03-22 00:26:18 -0400

    Fixed parsing scripts; got fits for exp2

--- lab4/datawork/p2A.m
+++ lab4/datawork/p2A.m
@@ -3,25 +3,30 @@ clearvars
 
 load exp2Adata.mat
 
+% Determine the Iy value for the according resistor
 IyA300 = Vin./R3;
 IyA3000 = Vin./R3k;
 IyA30000 = Vin./R30k;
 
+% Make labels for data
 lbl3 = sprintf("Data: I_y = %.8f", IyA300);
 lbl3k = sprintf("Data: I_y = %.8f", IyA3000);
 lbl30k = sprintf("Data: I_y = %.8f", IyA30000);
 
+% Make labels for fits
 flbl3 = sprintf("Fit: I_y = %.8f", IyA300);
 flbl3k = sprintf("Fit: I_y = %.8f", IyA3000);
 flbl30k = sprintf("Fit: I_y = %.8f", IyA30000);
 
+% Make theoretical data
 fIz300 = sqrt(IxA300.*IyA300);
 fIz3000 = sqrt(IxA3000.*IyA3000);
 fIz30000 = sqrt(IxA30000.*IyA30000);
 
-loglog(IxA300, IzA300, 'r.', ...
-       IxA3000, IzA3000, 'b.', ...
-       IxA30000, IzA30000, 'g.', ...
+% Plot away. Iz values are negative due to setup.
+loglog(IxA300, -IzA300, 'r.', ...
+       IxA3000, -IzA3000, 'b.', ...
+       IxA30000, -IzA30000, 'g.', ...
        IxA30000, fIz300, 'r', ...
        IxA30000, fIz3000, 'b', ...
        IxA30000, fIz30000, 'g')
--- lab4/datawork/p2B.m
+++ lab4/datawork/p2B.m
@@ -3,18 +3,34 @@ clearvars
 
 load exp2Bdata.mat
 
+% Determine the Ix value for the according resistor
 IxB300 = Vin./R3;
 IxB3000 = Vin./R3k;
 IxB30000 = Vin./R30k;
 
-lbl3 = sprintf("I_x = %.8f", IxB300);
-lbl3k = sprintf("I_x = %.8f", IxB3000);
-lbl30k = sprintf("I_x = %.8f", IxB30000);
+% Make labels for data
+lbl3 = sprintf("Data: I_x = %.8f", IxB300);
+lbl3k = sprintf("Data: I_x = %.8f", IxB3000);
+lbl30k = sprintf("Data: I_x = %.8f", IxB30000);
 
-loglog(IyB300, IzB300, 'r.', ...
-       IyB3000, IzB3000, 'b.', ...
-       IyB30000, IzB30000, 'g.')
+% Make labels for fits
+flbl3 = sprintf("Fit: I_x = %.8f", IxB300);
+flbl3k = sprintf("Fit: I_x = %.8f", IxB3000);
+flbl30k = sprintf("Fit: I_x = %.8f", IxB30000);
+
+% Make theoretical data. Iy values negative due to setup.
+fIz300 = sqrt(IxB300.*-IyB300);
+fIz3000 = sqrt(IxB3000.*-IyB3000);
+fIz30000 = sqrt(IxB30000.*-IyB30000);
+
+% Plot away. Iz values are negative due to setup.
+loglog(IyB300, -IzB300, 'r.', ...
+       IyB3000, -IzB3000, 'b.', ...
+       IyB30000, -IzB30000, 'g.',...
+       IyB300, fIz300, 'r', ...
+       IyB3000, fIz3000, 'b', ...
+       IyB30000, fIz30000, 'g')
 xlabel('I_y Current (A)')
 ylabel('I_z Current (A)')
-legend(lbl3, lbl3k, lbl30k)
+legend(lbl3, lbl3k, lbl30k, flbl3, flbl3k, flbl30k)
 title('I_z as a function of I_y for all I_x values')
