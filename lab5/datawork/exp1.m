20f5d16f261947ed02de7c3b10f365a87feedd2b
Shashank Swaminathan <sh.swami235@gmail.com>
2020-03-25 15:04:04 -0400

    Finished script for creating exp 1 plots

--- lab5/datawork/exp1.m
+++ lab5/datawork/exp1.m
@@ -1,3 +1,6 @@
+close all
+clearvars
+
 raw_data = LTspice2Matlab("./ltspice/lab5e1.raw");
 
 var = raw_data.variable_mat;
@@ -5,5 +8,25 @@ var = raw_data.variable_mat;
 Ip = var(12,:);
 In = var(13,:);
 Vin = var(2,:);
+Vdd = var(6,:);
 
-semilogy(Vin, Ip, Vin, In)
+[Is_n, V_T0_n, kn] = ekvfit(Vin, In, 0.0001);
+[Is_p, V_T0_p, kp] = ekvfit(Vdd-Vin, Ip, 0.0001);
+
+% For plot purposes
+% [Is_n, V_T0_n, k_n] = ekvfit(Vin, In, 0.0001, 'on');
+% [Is_p, V_T0_p, k_p] = ekvfit(Vdd-Vin, Ip, 0.0001, 'on');
+
+% Calculate g_m = del(I_sat) / del(V_g)
+gn = diff(In)./diff(Vin);
+gp = diff(Ip)./diff(Vin);
+
+fgnw = kn*In(2:end)./U_T;
+fgns = kn*sqrt(Is_n*In(2:end))/U_T;
+
+fgpw = kn*Ip(2:end)./U_T;
+fgps = kn*sqrt(Is_n*Ip(2:end))/U_T;
+
+loglog(In(2:end), gn, 'r.', Ip(2:end), gp, 'b.', ...
+       In(2:end), fgnw, 'r', In(2:end), fgns, 'y', ...
+       Ip(2:end), fgpw, 'b', Ip(2:end), fgps, 'c')
