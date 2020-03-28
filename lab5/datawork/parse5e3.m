238b49d4ce209c51ca75636e3543f6bc1eee7e95
Manu Patil <mpatil99@gmail.com>
2020-03-28 18:11:01 -0400

    Shashank pls help

--- lab5/datawork/parse5e3.m
+++ lab5/datawork/parse5e3.m
@@ -1,10 +1,10 @@
 close all
 clearvars
-addpath("../../GlobalFunctions");
+addpath(genpath("../../GlobalFunctions"));
 raw_datan = LTspice2Matlab("./ltspice/lab5e3n.raw");
 
 data =raw_datan.variable_mat;
-raw_datan.variable_name_list
+raw_datan.variable_name_list;
 figure();
 Vin = data(9,:,1);
 
@@ -13,28 +13,21 @@ Id7n = data(14,:,2);
 Id5n = data(14,:,3);
 
 
-[~, ~, , ~, ~] = linefit(Vs, log(nIsat), 0.001);
+[~, ~, r0inv6n, Isat6n, ~] = linefit(Vin(101:end), log(Id6n(101:end)), 0.001);
+r06n = 1/r0inv6n;
 
-
-figure();
-Vin = data(9,:,1);
-
-Id6n = data(14,:,1);
-Id7n = data(14,:,2);
-Id5n = data(14,:,3);
-
-semilogy(Vin, Id6n, 'r',...
-    Vin, Id7n, 'g',...
-    Vin, Id5n, 'b');
+semilogy(Vin(101:end), Id6n(101:end), 'r',...
+    Vin(101:end), Id7n(101:end), 'g',...
+    Vin(101:end), Id5n(101:end), 'b');
 title("nMOS Drain Characteristics", "Interpreter", "Latex");
-
+legend("6","7","5");
 
 figure(2);
 raw_data = LTspice2Matlab("./ltspice/lab5e3p.raw");
 
 data = raw_data.variable_mat;
 
-raw_data.variable_name_list
+raw_data.variable_name_list;
 
 Vin = data(9,:,1);
 
