df6c53340d280b26727220df02adebe9b2cf0eac
Manu Patil <mpatil99@gmail.com>
2020-03-27 22:12:07 -0400

    Creating library and proper links

---
+++ lab5/datawork/parse5e3.m
@@ -0,0 +1,36 @@
+close all
+clearvars
+addpath("../../GlobalFunctions");
+raw_datan = LTspice2Matlab("./ltspice/lab5e3n.raw");
+
+data = raw_datan.variable_mat;
+
+raw_datan.variable_name_list
+
+Vin = data(9,:,1);
+
+Id6n = data(14,:,1);
+Id7n = data(14,:,2);
+Id5n = data(14,:,3);
+
+semilogy(Vin, Id6n, 'r',...
+    Vin, Id7n, 'g',...
+    Vin, Id5n, 'b');
+
+
+figure(2);
+raw_data = LTspice2Matlab("./ltspice/lab5e3p.raw");
+
+data = raw_data.variable_mat;
+
+raw_data.variable_name_list
+
+Vin = data(9,:,1);
+
+Id8p = data(13,:,1);
+Id7p = data(13,:,2);
+Id0p = data(13,:,3);
+
+semilogy(Vin, Id8p, 'r',...
+    Vin, Id7p, 'g',...
+    Vin, Id0p, 'b');
--- lab5/datawork/parseTemplate.m
+++
@@ -1,5 +0,0 @@
-raw_data = LTspice2Matlab("./ltspice/lab5e1.raw");
-
-var = raw_data.variable_mat;
-
-var(1,:,:);
