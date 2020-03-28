7f1449768d59146c383fff70d32b1d440cfe939e
Manu Patil <mpatil99@gmail.com>
2020-03-28 17:26:21 -0400

    Adding global function

--- lab5/datawork/parse5e3.m
+++ lab5/datawork/parse5e3.m
@@ -3,10 +3,20 @@ clearvars
 addpath("../../GlobalFunctions");
 raw_datan = LTspice2Matlab("./ltspice/lab5e3n.raw");
 
-data = raw_datan.variable_mat;
-
+data =raw_datan.variable_mat;
 raw_datan.variable_name_list
+figure();
+Vin = data(9,:,1);
 
+Id6n = data(14,:,1);
+Id7n = data(14,:,2);
+Id5n = data(14,:,3);
+
+
+[~, ~, , ~, ~] = linefit(Vs, log(nIsat), 0.001);
+
+
+figure();
 Vin = data(9,:,1);
 
 Id6n = data(14,:,1);
@@ -16,6 +26,7 @@ Id5n = data(14,:,3);
 semilogy(Vin, Id6n, 'r',...
     Vin, Id7n, 'g',...
     Vin, Id5n, 'b');
+title("nMOS Drain Characteristics", "Interpreter", "Latex");
 
 
 figure(2);
@@ -34,3 +45,6 @@ Id0p = data(13,:,3);
 semilogy(Vin, Id8p, 'r',...
     Vin, Id7p, 'g',...
     Vin, Id0p, 'b');
+
+
+
