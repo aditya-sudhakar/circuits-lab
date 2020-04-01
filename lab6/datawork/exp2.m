59f934fcd6b0e540bcff38f3a8ea873e531848f6
Manu Patil <mpatil99@gmail.com>
2020-04-01 15:44:56 -0400

    Finished Plot work, need to double check with ninja on plots 3 and 4

--- lab6/datawork/exp2.m
+++ lab6/datawork/exp2.m
@@ -11,3 +11,39 @@ Isat_ser = load("./lab6/exp2/Isat_ser.txt");
 Vd_ohmic = load("./lab6/exp2/Vd_ohmic.txt");
 Vd_sat = load("./lab6/exp2/Vd_sat.txt");
 Vg = load("./lab6/exp2/Vg.txt");
+
+
+figure()
+semilogy(Vg, Iohmic, 'r',...
+    Vg, Iohmic_par, 'g',...
+    Vg, Iohmic_ser, 'b');
+title("Channel Current for $V_{DS}=10mV$", "Interpreter", "Latex");
+xlabel("Gate Voltage (V)", "Interpreter", "Latex");
+ylabel("Channel Current(A)", "Interpreter", "Latex");
+legend("Single nMOS", "Parallel Connection", "Series Connection", "Interpreter", "Latex");
+
+
+figure()
+semilogy(Vg, Isat, 'r',...
+    Vg, Isat_par, 'g',...
+    Vg, Isat_ser, 'b');
+title("Channel Current for $V_{DS}=V_{dd}$", "Interpreter", "Latex");
+xlabel("Gate Voltage (V)", "Interpreter", "Latex");
+ylabel("Channel Current(A)", "Interpreter", "Latex");
+legend("Single nMOS", "Parallel Connection", "Series Connection", "Interpreter", "Latex");
+
+figure()
+plot (Vg, Iohmic_par./Iohmic,...
+    Vg, Isat_par./Isat);
+title("Ratio between Parllel and Single", "Interpreter", "Latex");
+xlabel("Gate Voltage (V)", "Interpreter", "Latex");
+ylabel("Parallel:Single (unitless)", "Interpreter", "Latex");
+legend("Ratio in Ohmic Region", "Ratio in Saturation Region", "Interpreter", "Latex");
+
+figure()
+plot (Vg, Iohmic./Iohmic_ser,...
+    Vg, Isat./Isat_ser);
+title("Ratio between Single and Serial", "Interpreter", "Latex");
+xlabel("Gate Voltage (V)", "Interpreter", "Latex");
+ylabel("Single:Serial (unitless)", "Interpreter", "Latex");
+legend("Ratio in Ohmic Region", "Ratio in Saturation Region", "Interpreter", "Latex");
